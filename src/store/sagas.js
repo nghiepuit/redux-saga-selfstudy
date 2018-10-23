62edd8c80bb7819214f52ecb8a4526ffaf3873e8
nghiepuit <nghiep.phan@saigontechnology.com>
2018-10-23 17:50:03 +0700

    Replace fork by takeLatest without infinite loop

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,31 +1,28 @@
-import {fork, take, call, put} from "redux-saga/effects";
+import {call, put, takeLatest} from "redux-saga/effects";
 import {types} from './../redux/actions/TaskActionTypes';
 import * as api from './../api';
 
 export default function* rootSaga(getState) {
-  yield fork(watchFetchTasks);
+  yield takeLatest(types.FETCH_TASKS, fetchTasks);
 }
 /**
  * If FETCH_TASKS action called => this saga will called
  */
-function* watchFetchTasks() {
-  while(true) {
-    yield take(types.FETCH_TASKS);
-    try {
-      const {data} = yield call(api.fetchtasks);
-      yield put({
-        type: types.FETCH_TASKS_SUCCESS,
-        payload: {
-          tasks: data
-        }
-      });
-    } catch (e) {
-      yield put({
-        type: types.FETCH_TASKS_FAILED,
-        payload: {
-          error: e
-        }
-      });
-    }
+function* fetchTasks() {
+  try {
+    const {data} = yield call(api.fetchtasks);
+    yield put({
+      type: types.FETCH_TASKS_SUCCESS,
+      payload: {
+        tasks: data
+      }
+    });
+  } catch (e) {
+    yield put({
+      type: types.FETCH_TASKS_FAILED,
+      payload: {
+        error: e
+      }
+    });
   }
 }
